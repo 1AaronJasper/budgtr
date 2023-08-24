649c867c838cd468de6482d3e499e8e61f384bc4
Aaron Jasper <aaronjasper@Aarons-MacBook-Pro.local>
2023-08-24 00:51:53 -0500

    first push up

---
+++ sever.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const Budget = require("./models/budget.js")
+const expressLayouts = require('express-ejs-layouts');
+
+
+app.set('view engine', 'ejs');
+
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(expressLayouts)
+app.use(express.urlencoded({ extended:false}));
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+})
+
+
+app.listen(PORT, () => {console.log("this here is a port!")});
