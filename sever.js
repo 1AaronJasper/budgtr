1ba1614bb1fdc197b88e62a94a2c219ef0082e25
1AaronJasper <aaronjasper94@gmail.com>
2023-08-24 17:41:21 -0500

    finally

--- sever.js
+++ sever.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
-const Budget = require("./models/budget.js")
+const budget = require("./models/budget.js")
 const expressLayouts = require('express-ejs-layouts');
 
 
@@ -11,14 +11,51 @@ app.set('view engine', 'ejs');
 //MIDDLEWARES
 app.use(express.static('public'));
 app.use(expressLayouts)
-app.use(express.urlencoded({ extended:false}));
+app.use(express.urlencoded({ extended:true}));
 
-app.get('/', (req, res) => {
+
+//index
+app.get("/", (req, res) => {
     res.send("hello world");
-})
+});
 
-app.get("/index", (req, res) => {
-    res.render("index.ejs");
-})
+app.get("/budgets", (req, res) => {
+    res.render("index", { budget: budget })
+});
+
+//New
+
+app.get("/budgets/new", (req, res) => {
+    res.render("new.ejs");
+  });
+  
+
+// Create
+app.post("/budgets", (req, res) => {
+   
+    const newBudget = {
+        date: req.body.date,
+        from: req.body.from,
+        amount: req.body.amount,
+        tags: req.body.tags,
+    };
+
+    res.redirect("/budgets");
+});
+// Show
+app.get('/budgets/:id', (req, res) => {
+    res.render('show.ejs', {
+       budget: budget[req.params.id]
+    });
+});
+
+app.post('/submit', (req, res) => {
+    const formData = req.body; // Access form data here
+    // Process the form data as needed
+    res.send(`Received form data: ${JSON.stringify(formData)}`);
+});
+
+
+
 
 app.listen(PORT, () => {console.log("this here is a port!")});
